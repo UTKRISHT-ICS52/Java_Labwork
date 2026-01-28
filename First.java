31b1b45da3d8d3ee76d9d0156e65bf149db95f06
UTKRISHT-ICS52 <codewithutkrisht@gmail.com>
2026-01-28 12:15:24 +0530

    Java Lab work

---
+++ First.java
@@ -0,0 +1,6 @@
+// Program number :1 (Hello World Program)
+class First{
+public static void main(String[] args) {
+    System.out.println("Hello Utkrisht!");
+}
+}
---
+++ Second.java
@@ -0,0 +1,16 @@
+// Program Number: 2 (Sum of two numbers)
+public class Second {
+    public static void main(String[] args) {
+        int a = 10;
+        int b= 4;
+        int sum = a + b;
+        int Diff = a - b;
+        int multiply = a*b;
+        float division = (float)a / b;
+        System.out.println("sum of 5 and 10 is: " + sum);
+        System.out.println("difference of 5 and 10 is: " + Diff);
+        System.out.println("multiplication of 5 and 10 is: " + multiply);
+        System.out.println("division of 5 and 10 is: " + division);
+    }
+}
+    
