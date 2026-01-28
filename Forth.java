c766d2c98cde64d89eea63a3f3a2704450b4a490
UTKRISHT-ICS52 <codewithutkrisht@gmail.com>
2026-01-28 12:46:37 +0530

    Java Lab work 28.01.2026 Done by UTKRISHT

---
+++ Forth.java
@@ -0,0 +1,16 @@
+// Program number : 4(Largest of two numbers input by user)
+import java.util.Scanner;
+public class Forth {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        int a = sc.nextInt();
+        System.out.println("Enter second number:");
+        int b = sc.nextInt();
+        if (a > b) {
+            System.out.println("Largest number is: " + a);
+        } else {
+            System.out.println("Largest number is: " + b);
+        }
+    }
+}
---
+++ Third.java
@@ -0,0 +1,9 @@
+// Program number : 3 ( Student name , Branch , Roll no.)
+class Third{
+    public static void main(String[] args){
+        System.out.println("Name: Utkrisht Patel");
+        System.out.println("Branch: Computer Science");
+        System.out.println("Roll No: 245ICS052");
+
+    }
+}
