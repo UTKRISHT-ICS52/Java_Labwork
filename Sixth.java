129a4a990d1a6a7c23fd94c55116ccb665b01c3f
UTKRISHT-ICS52 <codewithutkrisht@gmail.com>
2026-02-18 00:38:42 +0530

    Operators.....practice

--- Sixth.java
+++ Sixth.java
@@ -1,14 +1,10 @@
 // Using Constants
-class Sixth {
+class ConstantExample {
     public static void main(String[] args) {
-        final int MAX_AGE = 100;          // constant integer
-        final double PI = 3.14159;       // constant decimal
-        final char GRADE_A = 'A';        // constant character
-        final boolean IS_ACTIVE = true;  // constant boolean
+        final double PI = 3.14159;  // constant value
+        final int MAX_USERS = 100;
 
-        System.out.println("Max Age: " + MAX_AGE);
         System.out.println("PI: " + PI);
-        System.out.println("Grade A: " + GRADE_A);
-        System.out.println("Is Active: " + IS_ACTIVE);
+        System.out.println("Max Users: " + MAX_USERS);
     }
 }
