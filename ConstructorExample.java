3faea3aa60e902f582e974df5ad39e32d0ca130a
UTKRISHT-ICS52 <codewithutkrisht@gmail.com>
2026-03-11 12:45:42 +0530

    Today Lab work is completed 11 March 2026

---
+++ ConstructorExample.java
@@ -0,0 +1,15 @@
+// create a class name ConstructorExample
+public class ConstructorExample {
+    // create a constructor
+    public ConstructorExample() {
+        System.out.println("This is a constructor example.");
+    }
+
+    // main method to create an object of the class
+    public static void main(String[] args) {
+        ConstructorExample example = new ConstructorExample();
+    }
+}
+
+
+
