225e1a05d80274fdee082cd7f70dac02d6b4d7e8
UTKRISHT-ICS52 <codewithutkrisht@gmail.com>
2026-04-15 18:41:12 +0530

    Changes are reuired 15-04-2026

--- EceptionDemo.java
+++ EceptionDemo.java
@@ -1,30 +1,42 @@
-class EceptionDemo {
-
-    static void checkNumber(int num) throws ArithmeticException {
+class ExceptionDemo {
+    static void checkNumber(int num) {
         if (num < 0) {
             throw new ArithmeticException("Negative number not allowed");
         }
     }
-
     public static void main(String[] args) {
+        // 1. Arithmetic Exception
         try {
-            int a = 10 / 0;  // Arithmetic Exception
-
-            int arr[] = new int[3];
-            arr[5] = 10;    // ArrayIndexOutOfBoundsException
-
-            Object x[] = new String[2];
-            x[0] = new Integer(10);  // ArrayStoreException
-
-            checkNumber(-5);
-
+            int a = 10 / 0;
         } catch (ArithmeticException e) {
             System.out.println("Arithmetic Exception caught: " + e);
+        }
+
+        // 2. Array Index Out Of Bounds
+        try {
+            int arr[] = new int[3];
+            arr[5] = 10;
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Array Index Out Of Bounds Exception caught: " + e);
+        }
+
+        // 3. Array Store Exception
+        try {
+            Object x[] = new String[2];
+            x[0] = Integer.valueOf(10);   // fixed (modern way)
         } catch (ArrayStoreException e) {
             System.out.println("Array Store Exception caught: " + e);
-        } finally {
+        }
+
+        // 4. Custom Exception
+        try {
+            checkNumber(-5);
+        } catch (ArithmeticException e) {
+            System.out.println("Custom Exception caught: " + e);
+        }
+
+        // Finally block
+        finally {
             System.out.println("Finally block executed");
         }
     }
